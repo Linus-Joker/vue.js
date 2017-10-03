d196b5e09e7f8f6901b8eaa69d5583434c74b4e7
milk <milk790513@yahoo.com.tw>
2017-10-03 13:59:39 +0800

    Update vue-model.js

--- vue-model.js
+++ vue-model.js
@@ -4,9 +4,12 @@ HTML部分:
   <div id="app">
     
     輸入台幣: <input type="text" v-model="NT">元
+
    <ul>
      <li>日幣:{{NT / 0.2719}}元</li>
-     <lu>美金:{{NT / 30.657}}元</lu>
+     <li>美金:{{NT / 30.657}}元</li>
+     <li>人民幣:{{NT / 4.623}}元</li>
+     <li>港幣:{{NT / 3.945}}元</li>
    </ul>
     
   </div>
