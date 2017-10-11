0619ea49b6ef9ed80493d57e5a6f6e129eb08667
milk <milk790513@yahoo.com.tw>
2017-10-11 14:59:29 +0800

    Create vue-做數量加減.js

---
+++ vue-做數量加減.js
@@ -0,0 +1,38 @@
+HTML 部分
+
+<body>
+  <div id="app">{{message}}
+   
+    <input type="datalist" v-model="number">
+    <input type="button" value="+" v-on:click="add()">
+    <input type="button" value="-" v-on:click="sub()"><br>
+    {{number}}
+    
+  </div>
+
+  
+  <script src="https://unpkg.com/vue@2.4.4/dist/vue.js"></script>
+</body>
+
+
+JS部分
+
+var app = new Vue({
+  el:'#app',
+  data:{
+    
+    message:'請點選數量!!',
+    number:1
+  },
+  methods:{
+    add:function(){
+     this.number = this.number + 1;
+      console.log(this.number);     
+    },
+    sub:function(){
+      this.number = this.number -1;
+    }
+      
+  }
+    
+})
